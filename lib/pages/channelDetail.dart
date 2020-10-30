6c4a3fb17773ac31f80dbdf3bf2dbbae890d7c1b
flex <814114037@qq.com>
2020-10-31 00:59:25 +0800

    channel list detail

--- lib/pages/channelDetail.dart
+++ lib/pages/channelDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class myApp extends StatelessWidget{
   final String channelId;
@@ -12,13 +13,30 @@ class myApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('栏目详情'),
-      ),
-      body: channelBody()
-      // home: homeTop.myApp(),
+    return MaterialApp(
+        title: 'channel',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('栏目详情'),
+            ),
+            body: contentList(list: [
+              {'title' : '11111111111111111111111111111111111111111111111111', 'id': '1'},
+              {'title' : '222222', 'id': '2'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '33333', 'id': '3'},
+              {'title' : '2', 'id': '3'},
+              {'title' : '234', 'id': '3'},
+              {'title' : 'dsfsdds', 'id': '3'},
+              {'title' : '44444444', 'id': '4'}
+            ])
+          // home: homeTop.myApp(),
+        )
     );
+    // return
   }
 }
 
@@ -26,28 +44,38 @@ class channelBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    // return MaterialApp()
+    return Wrap(
+      direction: Axis.horizontal,
+        // crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              channelDetailTop(
-                coverImg: 'https://pcdn.flutterchina.club/imgs/book.png',
-                title: '栏目名字',
-                descriptiton: 'descriptiton'
-              )
-              // Text(" I am Jack "),
-            ]
-        ),
-        channelContentList()
         // Row(
         //     crossAxisAlignment: CrossAxisAlignment.start,
-        //     children: <Widget>[
-        //       Text("111"),
+        //     children: [
+        //       channelDetailTop(
+        //         coverImg: 'https://pcdn.flutterchina.club/imgs/book.png',
+        //         title: '栏目名字',
+        //         descriptiton: 'descriptiton'
+        //       )
         //       // Text(" I am Jack "),
         //     ]
         // ),
+        // channelContentList()
+        contentList(list: [
+          {'title' : '11111111111111111111111111111111111111111111111111', 'id': '1'},
+          {'title' : '222222', 'id': '2'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '33333', 'id': '3'},
+          {'title' : '2', 'id': '3'},
+          {'title' : '234', 'id': '3'},
+          {'title' : 'dsfsdds', 'id': '3'},
+          {'title' : '44444444', 'id': '4'}
+        ])
+
       ],
     );
   }
@@ -104,9 +132,17 @@ class channelContentList extends StatelessWidget{
     return Container(
       margin: EdgeInsets.only(top: 10.0, right: 25.0, left: 10.0), //容器外填充
       child: contentList(list: [
-        {'title' : '11111', 'id': '1'},
+        {'title' : '11111111111111111111111111111111111111111111111111', 'id': '1'},
         {'title' : '222222', 'id': '2'},
         {'title' : '33333', 'id': '3'},
+        {'title' : '33333', 'id': '3'},
+        {'title' : '33333', 'id': '3'},
+        {'title' : '33333', 'id': '3'},
+        {'title' : '33333', 'id': '3'},
+        {'title' : '33333', 'id': '3'},
+        {'title' : '2', 'id': '3'},
+        {'title' : '234', 'id': '3'},
+        {'title' : 'dsfsdds', 'id': '3'},
         {'title' : '44444444', 'id': '4'}
       ])
     );
@@ -123,52 +159,82 @@ class contentList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 下划线widget预定义以供复用。
-    return ListView(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(10.0),
-      children: _listView(context),
+    // return Scaffold(
+    //     body: SafeArea(
+    //       child: ListView(
+    //         shrinkWrap: true,
+    //         // physics: const AlwaysScrollableScrollPhysics(),
+    //         padding: const EdgeInsets.all(10.0),
+    //         children: _listView(context),
+    //       ),
+    //     ),
+    // );
+    return Container(
+      child: ListView(
+        shrinkWrap: true,
+        // physics: const AlwaysScrollableScrollPhysics(),
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          channelDetailTop(
+              coverImg: 'https://pcdn.flutterchina.club/imgs/book.png',
+              title: '栏目名字',
+              descriptiton: 'descriptiton'
+          ),
+          for (final item in list)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                  alignment: Alignment.centerLeft, //卡片内文字居中
+                  height: 50.0,
+                  // margin: EdgeInsets.only(top:10.0, left: 10.0), //容器外填充
+                  child:  FlatButton(
+                    child: Text(
+                      item['title'],
+                      style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      print(item['id']);
+                    },
+                  )
+              ),
+            ),
+          // _listView(context),
+        ]
+      ),
     );
   }
+
   List<Widget> _listView(context){
     return list.map((f)=>
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Center(
-                      child: Container(
-                        height: 30.0,
-                        margin: EdgeInsets.only(top:10.0, left: 10.0), //容器外填充
-                        child:  Text('${f['id']}',
-                            style: TextStyle(color: Colors.black,fontSize: 16)),
-                      )
+        Wrap(
+          alignment: WrapAlignment.start,
+          // direction: Axis.horizontal,
+          children: [
+            // Container(
+            //   height: 30.0,
+            //   margin: EdgeInsets.only(top:10.0, left: 10.0), //容器外填充
+            //   child:  Text('${f['id']}',
+            //       style: TextStyle(color: Colors.black,fontSize: 16)),
+            // ),
+            Container(
+              child: Container(
+                  alignment: Alignment.centerLeft, //卡片内文字居中
+                  height: 50.0,
+                  // margin: EdgeInsets.only(top:10.0, left: 10.0), //容器外填充
+                  child:  FlatButton(
+                    child: Text(
+                      f['title'],
+                      style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),
                     ),
-                    Center(
-                      child: Container(
-                          height: 30.0,
-                          // margin: EdgeInsets.only(top:10.0, left: 10.0), //容器外填充
-                          child:  FlatButton(
-                            child: Text(
-                              f['title'],
-                              style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: () {
-                              print(f['id']);
-                              //导航到新路由
-                              // Navigator.push( context,
-                              //     MaterialPageRoute(builder: (context) {
-                              //       // return channelDetail.myApp();
-                              //     }));
-                            },
-                          )
-                      ),
-                    ),
-
-                  ],
-                ),
-                Divider(color: Colors.black26,)
-              ],
-            )
+                    onPressed: () {
+                      print(f['id']);
+                    },
+                  )
+              ),
+            ),
+            Divider(color: Colors.black26,)
+          ],
+        ),
     ).toList();
   }
 }
